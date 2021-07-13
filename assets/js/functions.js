27ce4f0c8764832df3b2dd0671926ac4f557ad6c
Pere Serra Bosch <pere.serra.bosch@gmail.com>
2021-07-13 14:58:46 +0200

    clean code

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,9 +1,30 @@
 'use strict';
 
 function startGame() {
+  let fiveMinutes = 60 * 1,
+    display = document.querySelector('#time');
+  startTimer(fiveMinutes, display);
+  console.log(display);
 
+  moveMainSection();
 }
 
+function startTimer(duration, display) {
+  let timer = duration, minutes, seconds;
+  setInterval(function () {
+    minutes = parseInt(timer / 60, 10)
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds;
+
+    if (--timer < 0) {
+      timer = duration;
+    }
+  }, 1000);
+}
 class FormValidator {
   constructor(form, fields) {
     this.form = form
@@ -26,7 +47,7 @@ class FormValidator {
         user[`${field}`] = input.value;
       });
 
-      moveMainSection();
+      startGame()
     });
 
   }
